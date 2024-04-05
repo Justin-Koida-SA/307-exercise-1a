ddba3b96c760f88a97a2c8df155ba0d1115783ef
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2024-04-05 11:38:52 -0700

    Step 9 commit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,11 @@
 // src/MyApp.jsx
 import React, {useState} from "react";
 import Table from "./Table";
+import Form from "./Form";
 
   
   function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-          name: "Charlie",
-          job: "Janitor" // the rest of the data
-        },
-        {
-            name: "Mac",
-            job: "Bouncer"
-          },
-          {
-            name: "Dee",
-            job: "Aspring actress"
-          },
-          {
-            name: "Dennis",
-            job: "Bartender"
-          }
-      ]);
+    const [characters, setCharacters] = useState([]);
       
     
       function removeOneCharacter(index) {
@@ -32,7 +16,11 @@ import Table from "./Table";
       }
       return (
         <div className="container">
-          <Table characterData={characters} />
+          <Table 
+            characterData={characters} 
+            removeCharacter={removeOneCharacter}  
+          />
+          <Form />
         </div>
       );
 
