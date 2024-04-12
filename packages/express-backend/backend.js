c4dac8aced87e79cd810e3ee9dd80284c88257c1
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2024-04-11 17:56:17 -0700

    Update backend.js
    fixed

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 8008;
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+});
+
 const users = {
     users_list: [
       {
@@ -32,25 +44,30 @@ const users = {
     ]
   };
 
+
+  app.get("/users", (req, res) => {
+    res.send(users);
+  });
+  
   const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
   };
-
-  const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-  const addUser = (user) => {
-    users["users_list"].push(user);
-    return user;
-  };
   
-  app.post("/users", (req, res) => {
-    const userToAdd = req.body;
-    addUser(userToAdd);
-    res.send();
+  app.get("/users", (req, res) => {
+  const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
   });
+
+const findUserById = (id) =>
+users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
@@ -61,31 +78,16 @@ app.get("/users/:id", (req, res) => {
     res.send(result);
   }
 });
-  
-  app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
-    } else {
-      res.send(users);
-    }
-  });
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
 
-  app.get("/users", (req, res) => {
-    res.send(users);
-  });
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
 
-app.use(express.json);
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
-  
-  app.listen(port, () => {
-    console.log(
-      `Example app listening at http://localhost:${port}`
-    );
-  });
 
