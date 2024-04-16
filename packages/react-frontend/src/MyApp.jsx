e15baf57e9657ae3c81d2a8ad5b65de62be6bcc5
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2024-04-16 15:18:06 -0700

    part 3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -7,7 +7,7 @@ import Form from "./Form";
   
   function MyApp() {
     const [characters, setCharacters] = useState([]);
-    
+
     useEffect(() => {
       fetchUsers()
         .then((res) => res.json())
@@ -21,14 +21,35 @@ import Form from "./Form";
         });
         setCharacters(updated);
       }
-      function updateList(person) {
-        setCharacters([...characters, person]);
+      // function updateList(person) {
+      //   setCharacters([...characters, person]);
+      // }
+
+      function updateList(person) { 
+        postUser(person)
+          .then(() => setCharacters([...characters, person]))
+          .catch((error) => {
+            console.log(error);
+          })
       }
 
       function fetchUsers() {
         const promise = fetch("http://localhost:8000/users");
         return promise;
       }
+      function postUser(person) {
+        const promise = fetch("Http://localhost:8000/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(person),
+        });
+    
+        return promise;
+      }
+
+  
       return (
         <div className="container">
           <Table 
